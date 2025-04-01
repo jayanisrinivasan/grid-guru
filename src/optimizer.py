234f316b64252703d6709b29ebe66a55778db2cd
Jayani Srinivasan <srinivasan.jayani@gmail.com>
2025-03-31 17:29:25 -0700

    Update optimizer.py

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,9 +1,24 @@
-# src/optimizer.py
+# src/optimizer.py (Upgraded)
 
-def optimize_dispatch(net_energy):
+def optimize_dispatch(net_energy, max_battery=5, min_battery=0, efficiency=0.9, peak_price=0.30):
     dispatch = []
+    grid_draw = []
+    cost = []
     battery = 0
-    for net in net_energy:
-        battery += net
+
+    for i, net in enumerate(net_energy):
+        if net >= 0:
+            battery = min(battery + net * efficiency, max_battery)
+        else:
+            need = abs(net)
+            if battery >= need:
+                battery -= need / efficiency
+                grid = 0
+            else:
+                grid = need - battery
+                battery = 0
+            cost.append(grid * peak_price)
+            grid_draw.append(grid)
         dispatch.append(battery)
-    return dispatch
+
+    return dispatch, grid_draw, cost
